18367b1de1a86298186964d610425eb68ab497f4
Andrew <expandrew@gmail.com>
2017-07-06 20:28:57 -0700

    make progress but lose place in lesson. Need to start again from last commit.

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
 
-import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
 @Component({
   selector: 'hero-detail',
   template: `
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
       <div>
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
